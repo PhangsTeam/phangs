5f3eab11715fdca8645f84c1da2f7a1bc31a643d
Erik Rosolowsky <rosolowsky@ualberta.ca>
2020-01-21 18:25:03 +0000

    Add rotation curve table

--- phangs/utilities.py
+++ phangs/utilities.py
@@ -8,15 +8,12 @@ from .phangs_galaxies import PhangsGalaxy
 
 __all__ = ['convolve_to_resolution']
 
-def convolve_to_resolution(filename, resolution=100 * u.pc, **kwargs):
+def convolve_to_resolution(filename, name=None,
+                           resolution=100 * u.pc, **kwargs):
     """
     Convolves an FITS image to a given linear resolution from a given distance sample table
     """    
     
-    if type(filename) is list:
-        for thisfile in filename:
-            convolve_to_resolution(thisfile, resolution=resolution, **kwargs)
-            return()
     hdr = fits.getheader(filename)  
     w = wcs.WCS(hdr)
     if w.naxis == 3:
@@ -25,6 +22,11 @@ def convolve_to_resolution(filename, resolution=100 * u.pc, **kwargs):
         data = Projection.from_hdu(fits.open(filename)[0])
     gal = PhangsGalaxy(data.header['OBJECT'])
     target_resn = (resolution / gal.distance * u.rad).to(u.arcsec)
-    target_beam = Beam(major=target_resn, minor=target_resn, pa=0 * u.deg)
+    target_beam = Beam(major=target_resn,
+                       minor=target_resn,
+                       pa=0 * u.deg)
     data_out = data.convolve_to(target_beam)
-    
+    if name is None:
+        resn_str = '_' + resolution.to_string().replace(' ','')
+        name = filename.replace('.fits', resn_str+'.fits')
+    data_out.write(name, overwrite=True)
