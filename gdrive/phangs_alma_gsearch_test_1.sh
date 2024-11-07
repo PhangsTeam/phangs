01a65c6ceeb42c359dc2b70165879d518209f156
Erik Rosolowsky <rosolowsky@ualberta.ca>
2024-11-07 00:07:02 +0000

    updates to package manager

--- gdrive/phangs_alma_gsearch_test_1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# 
-
-$(dirname ${BASH_SOURCE[0]})/phangs_alma_gsearch.py "/Archive/ALMA/delivery_v3p3/broad_maps/ngc3627_12m+7m*_mom0.fits" -verbose | tee phangs_alma_gsearch_test_1.log
-
